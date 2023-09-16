080c8bb1407430036a59916ba2714c2dd1dbc92e
rere <regis-graptin@sfr.fr>
2023-09-16 09:55:46 +0200

    change network to goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,12 @@ module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
-  }
+  },
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
